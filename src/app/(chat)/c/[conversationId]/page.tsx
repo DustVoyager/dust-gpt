94a815ff320f1e90d710e617620c2d343b55147d
DustVoyager <ohjamdol@gmail.com>
2024-10-29 21:32:26 +0900

    next 15 비동기 props 수정

--- src/app/(chat)/c/[conversationId]/page.tsx
+++ src/app/(chat)/c/[conversationId]/page.tsx
@@ -1,15 +1,10 @@
 import { getMessagesByConversation } from "@/data/conversations";
 import { Chat } from "@/src/components/chat/Chat";
 
-type Props = {
-  params: {
-    conversationId: string;
-  };
-};
+type Params = Promise<{ conversationId: string }>;
 
-export default async function ConversationPage({
-  params: { conversationId },
-}: Props) {
+export default async function ConversationPage({ params }: { params: Params }) {
+  const { conversationId } = await params;
   const messages = await getMessagesByConversation(conversationId);
   return <Chat initialMessages={messages} />;
 }
